c3de2a9a73ce178f16bd18345888ccabdd6e7d26
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-01-21 22:10:36 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pattern = bracketsConfig.map(el => el.join(''));
+  let max = str.length / 2;
+  let x = 0;
+  while (str && x < max) {
+    for (let i of pattern) {
+      str = str.replaceAll(i, '');
+      }
+      x++;
+  }
+  return !str ? true : false;
 }
