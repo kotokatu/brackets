6ab14edc8eadbb7e76cb8aad4ebf534dd0afc8e0
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-01-22 01:27:52 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = function check(str, bracketsConfig) {
-  str.split('').forEach(el => bracketsConfig.map(el => el.join('')).forEach(el => str = str.replaceAll(el, '')));
+  str.split('').forEach(() => bracketsConfig.map(el => el.join('')).forEach(el => str = str.replaceAll(el, '')));
   return !str ? true : false;
 }
